8f5921c559438be6d2d8f67008ead58113460c0f
MMNfahmi <e41221004@student.polije.ac.id>
2023-02-08 19:24:13 +0700

    Create Workshop_TIF.java

---
+++ testimoni/Workshop_TIF.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sinau.workshop_tif;
+
+/**
+ *
+ * @author DELL
+ */
+public class Workshop_TIF {
+
+    public static void main(String[] args) {
+        String nama = "fahmi";
+        char abjad = 'a';
+        int a = 18;
+        
+        System.out.println(nama);
+        System.out.println(a);
+        
+    }
+}
