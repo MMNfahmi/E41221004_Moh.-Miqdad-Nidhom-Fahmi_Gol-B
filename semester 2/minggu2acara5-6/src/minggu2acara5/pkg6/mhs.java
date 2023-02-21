b490b608a8c4b04c9a3764052d6a1b02d0994e66
MMNfahmi <e41221004@student.polije.ac.id>
2023-02-21 22:14:14 +0700

    Create mhs.java

---
+++ semester 2/minggu2acara5-6/src/minggu2acara5/pkg6/mhs.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu2acara5.pkg6;
+
+public class mhs {
+
+    public static void main(String[] args) {
+        String fName = "Moh. Miqdad Nidhom Fahmi";
+        int stuId = 208735326;
+        String stuStatus = "Active";
+        
+        System.out.println("Nama Mahasiswa      : "+fName);
+        System.out.println("ID Mahasiswa        : "+stuId);
+        System.out.println("Status Mahasiswa    : "+stuStatus);
+    }
+    
+}
