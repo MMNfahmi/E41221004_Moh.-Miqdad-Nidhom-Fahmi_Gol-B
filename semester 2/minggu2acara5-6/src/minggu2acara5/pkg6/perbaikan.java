d046802b600a573d5ea03f00276c14be1cf2d2b8
MMNfahmi <e41221004@student.polije.ac.id>
2023-02-21 22:14:28 +0700

    Create perbaikan.java

---
+++ semester 2/minggu2acara5-6/src/minggu2acara5/pkg6/perbaikan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu2acara5.pkg6;
+
+/**
+ *
+ * @author DELL
+ */
+public class perbaikan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean gameOver = false;
+        int students = 50, classes = 3;
+        double sales_tax;
+        short number1;
+        
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+    }
+    
+}
