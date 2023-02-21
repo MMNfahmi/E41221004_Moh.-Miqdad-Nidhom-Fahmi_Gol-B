1176f82c5eed5e80f4b3f7ceb71c25ee1a3d114c
MMNfahmi <e41221004@student.polije.ac.id>
2023-02-21 22:14:34 +0700

    Create person.java

---
+++ semester 2/minggu2acara5-6/src/minggu2acara5/pkg6/person.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu2acara5.pkg6;
+
+/**
+ *
+ * @author DELL
+ */
+public class person {
+    String nama;
+    int umur;
+    
+    public person(String nama, int umur){
+        this.nama = nama;
+        this.umur = umur;
+        
+    }
+    String getNama(){
+        return nama;
+    }
+    int getumur (){
+        return umur;
+    }
+
+    
+}
