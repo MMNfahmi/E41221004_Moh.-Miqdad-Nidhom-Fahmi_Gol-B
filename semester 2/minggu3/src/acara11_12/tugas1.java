e8bcd84d48d35c76010cef965fb52840d8982950
MMNfahmi <e41221004@student.polije.ac.id>
2023-02-21 23:21:27 +0700

    Create tugas1.java

---
+++ semester 2/minggu3/src/acara11_12/tugas1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acara11_12;
+
+/**
+ *
+ * @author DELL
+ */
+public class tugas1 {
+
+    public static void main(String[] args) {
+        String [] nama = {"Moh.","Miqdad","Nidhom","Fahmi"};
+        
+        for (int j = 0; j < nama.length; j++){
+            System.out.print(nama[j]);
+            System.out.print(" ");
+        }
+    }
+    
+}
